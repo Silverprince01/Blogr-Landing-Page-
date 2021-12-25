309548eeef5d52cb0bb8966e1d631b33b8f95bf3
Badru <badrufuadalabi@gmail.com>
2021-12-25 21:30:28 +0100

    commited all the necessary files to push

---
+++ main.js
@@ -0,0 +1,41 @@
+const product = document.getElementById('product');
+const produc = document.getElementById('produc');
+const company = document.getElementById('company');
+const compan = document.getElementById('compan');
+const contact = document.getElementById('contact');
+const connec = document.getElementById('connec');
+const hamburge = document.querySelector('.hamburger');
+const nav = document.querySelector('nav');
+const head = document.getElementById('Head');
+const act = product.addEventListener('click', () =>{
+    produc.classList.toggle('active');
+});
+company.addEventListener('click' , () =>{
+    compan.classList.toggle('active');
+});
+contact.addEventListener('click' , () =>{
+    connec.classList.toggle('active');
+});
+hamburge.addEventListener('click' , () =>{
+    hamburge.classList.toggle('active');
+    nav.classList.toggle('active');
+});
+document.querySelectorAll('#produc').forEach(n => n.addEventListener('click', () =>{
+    produc.classList.remove('active');
+
+}));
+document.querySelectorAll('#compan').forEach(n => n.addEventListener('click', () =>{
+    compan.classList.remove('active');
+    
+}));
+document.querySelectorAll('#connec').forEach(n => n.addEventListener('click', () =>{
+    
+    connec.classList.remove('active');
+    
+}));
+
+head.addEventListener('click', () =>{
+    produc.classList.remove('product');
+//     connec.classList.toggle('active')
+//     compan.classList.toggle('active')
+});
